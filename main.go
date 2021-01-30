ec84bf1df0a43f947cefd96c7463e07ee6dbe7e1
swimming_kim <tge1375@naver.com>
2021-01-30 15:51:37 +0900

    Upto 1_5
    Signed-off-by: swimming_kim <tge1375@naver.com>

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// you must specify the type of variables in function
+// also needs to specify return types too.
+func multiply(a int, b int) int {
+	return a * b
+}
+
+// function with multiple returns
+func lenAndUpper(name string) (int, string) {
+	// defer runs right after this function done
+	defer fmt.Println("lenAndUpper done... ")
+	return len(name), strings.ToUpper(name)
+}
+
+func superAdd(numbers ...int) int {
+	total := 0
+
+	for _, val := range numbers {
+		total += val
+		fmt.Print(val, " ")
+	}
+	fmt.Println()
+
+	return total
+}
+
+// naked return function
+func nakedLenAndUpper(name string) (length int, upperName string) {
+	length = len(name)
+	upperName = strings.ToUpper(name)
+	return
+}
+
+// function with multiple arguments
+func multipleArgs(words ...string) {
+	fmt.Println(words)
+}
+
+func main() {
+	const name string = "Swimming"
+	// name = "Changed" => invalid, const variable
+
+	var name2 string = "Jelly"
+	same_name := "Swimming" // same with this => var same_name string = "Swimming"
+	name2 = "Belly"         // => possible
+
+	fmt.Println(name2)
+	fmt.Println(same_name)
+
+	fmt.Println(multiply(2, 2))
+
+	const friend_name string = "James"
+
+	// you can ignore return values with underscore _
+	nameLen, upperName := lenAndUpper(friend_name)
+	fmt.Println(nameLen, upperName)
+
+	multipleArgs("This ", "is ", "test ", "sentences")
+
+	summation := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(summation)
+}
