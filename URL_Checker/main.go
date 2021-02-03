66359d6308240afce53e65f7d854e2b69003470f
swimming_kim <tge1375@naver.com>
2021-02-03 22:40:01 +0900

    Upto 3_7
    Signed-off-by: swimming_kim <tge1375@naver.com>

--- URL_Checker/main.go
+++ URL_Checker/main.go
@@ -6,10 +6,14 @@ import (
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 var urlErr = errors.New("URL Get Failed!!")
 
-func hitURL(url string) error {
-	fmt.Println("Checking : ", url)
+func hitURLNormal(url string) error {
 	res, err := http.Get(url)
 
 	if err != nil || res.StatusCode >= 400 {
@@ -19,23 +23,24 @@ func hitURL(url string) error {
 	return nil
 }
 
-func main() {
-	var results = make(map[string]string)
+func hitURLGoRoutine(url string, c chan<- requestResult) {
+	res, err := http.Get(url)
 
-	urls := []string{
-		"https://www.naver.com/",
-		"https://www.google.com/",
-		"https://www.facebook.com/",
-		"https://www.amazon.com/",
-		"https://www.netflix.com/",
-		"https://www.soundcloud.com/",
-		"https://www.notion.com/",
-		"https://www.helloworld.com/",
+	status := "OK"
+	if err != nil || res.StatusCode >= 400 {
+		status = "FAIL"
 	}
+	c <- requestResult{url: url, status: status}
+}
+
+func withoutGoRoutine(urls []string) {
+	results := make(map[string]string)
+
+	fmt.Println("Waiting for URL Get...")
 
 	for _, url := range urls {
 		result := "OK"
-		err := hitURL(url)
+		err := hitURLNormal(url)
 		if err != nil {
 			result = "FAIL"
 		}
@@ -45,4 +50,41 @@ func main() {
 	for url, result := range results {
 		fmt.Println(url, result)
 	}
+}
+
+func awesomeGoRoutine(urls []string) {
+	results := make(map[string]string)
+	c := make(chan requestResult)
+
+	for _, url := range urls {
+		go hitURLGoRoutine(url, c)
+	}
+
+	fmt.Println("Waiting for URL Get...")
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+func main() {
+
+	urls := []string{
+		"https://www.naver.com",
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.amazon.com",
+		"https://www.netflix.com",
+		"https://www.soundcloud.com",
+		"https://www.notion.com",
+		"https://www.helloworld.com",
+	}
+
+	withoutGoRoutine(urls)
+	awesomeGoRoutine(urls)
 }
